936a2ac6630bf818bd4204a60e02f111a7b98620
Antonio Cancela <antonio.cancela.diaz@gmail.com>
2016-10-23 20:07:33 +0200

    Primer commit y creación del proyecto.

---
+++ PhootShoot/urls.py
@@ -0,0 +1,38 @@
+"""football URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from PhootShoot.views import landing_page
+
+urlpatterns = [
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', landing_page),
+	url(r'^register/$', landing_page),
+	url(r'^login/$', landing_page),
+	url(r'^user/$', landing_page),
+	url(r'^team/$', landing_page),
+	url(r'^team/(\d{1,10})/$', landing_page),
+	url(r'^team/stadium/$', landing_page),
+	url(r'^team/lineup/$', landing_page),
+	url(r'^market/$', landing_page),
+	url(r'^competitions/$', landing_page),
+	url(r'^league/$', landing_page),
+	url(r'^league/(\d{1,8})/$', landing_page),
+	url(r'^league/(\d{1,8})/round/(\d{4})/(\d{1,2})/$', landing_page),
+	url(r'^player/(\d{1,12})/$', landing_page),
+	url(r'^shop/$', landing_page),
+	url(r'^payment/$', landing_page),
+]
---
+++ PhootShoot/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse 
+ 
+HTML = """ 
+<!DOCTYPE html> 
+<html lang="es"> 
+<head> 
+<meta http­equiv="content­type" content="text/html;  charset=utf­8"> 
+<meta name="robots" content="NONE,NOARCHIVE">
+<title>PhootShoot - El manager de fútbol online que siempre deseaste</title> 
+<style type="text/css"> 
+html * { padding:0; margin:0; } 
+body * { padding:10px 20px; } 
+body * * { padding:0; } 
+body { font:small sans­serif; } 
+body>div { border­bottom:1px solid #ddd; } 
+h1 { font­weight:normal; } 
+#summary { background: #e0ebff; } 
+</style> 
+</head> 
+<body> 
+<div id="presentation"> 
+<h1><strong>PhootShoot</strong> es el manager de fútbol con el que ganar dinero mientras te diviertes</h1> 
+</div> 
+</body></html> """ 
+ 
+def landing_page(request): 
+    return HttpResponse(HTML) 
